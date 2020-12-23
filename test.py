b20ef1a767f6814d54e94e2fa4bd8bdc57777c0a
Vincent Trau <trauvincent@gmail.com>
2020-12-22 20:09:51 -0800

    cleaned item files

--- test.py
+++ test.py
@@ -9,15 +9,15 @@ class TestTesseract(unittest.TestCase):
         for image in dictionary:
             img = cv2.imread(image)
             #img = cv2.bitwise_not(img)
-            img = cv2.blur(img, (5,5))
-            #img = cv2.GaussianBlur(img,(7,7),0)
+            #img = cv2.blur(img, (5,5))
+            img = cv2.GaussianBlur(img,(5,5),0)
             #kernel = np.ones((2,2), np.uint8)
             #img = cv2.dilate(img, kernel, iterations=1)
             #img = cv2.erode(img, kernel, iterations = 1)
             #cv2.imshow("image", img)
             #cv2.waitKey(0)
             whitelist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890(&!+,):-+[%]./\\'\\ "
-            custom_config = f"-c tessedit_char_whitelist={whitelist} --psm 6 -l itemFast"
+            custom_config = f"-c tessedit_char_whitelist={whitelist} --psm 6 -l engFast"
             ocr = pytesseract.image_to_string(img, config = custom_config)
             ocr = ocr.strip()
 
